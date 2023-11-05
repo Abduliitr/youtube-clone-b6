3e4e2d8002e94fa77cfff9bd383060853fc5a003
Abduliitr <abdul.thomso@gmail.com>
2023-11-06 00:28:36 +0530

    Hamburger++

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -3,6 +3,8 @@ import React from 'react'
 import ytLogo from './images/yt-logo.png'
 import searchIcon from './images/search.png'
 import {BsFillMicFill} from 'react-icons/bs'
+import {BiSearch} from 'react-icons/bi'
+import {GiHamburgerMenu} from 'react-icons/gi'
 import { Link } from 'react-router-dom'
 
 function Header() {
@@ -10,7 +12,9 @@ function Header() {
     <div>
       <div class="header">
                 <div class="header-items header-logo">
-                    <div class="header-first"></div>
+                    <div class="header-first">
+                        <GiHamburgerMenu />
+                    </div>
                     <div class="header-second">
                         {/* <a href='/'><img id='yt-logo' src={ytLogo}/></a> */}
                         <Link to={'/'}><img id='yt-logo' src={ytLogo}/></Link>
@@ -20,14 +24,16 @@ function Header() {
                     {/* <div class="header-search"></div> */}
                     <input class="header-search" placeholder='Search..'/>
                     <button class="search-button">
-                        <img class="small-image" src={searchIcon}></img>
+                        {/* <img class="small-image" src={searchIcon}></img> */}
+                        <BiSearch class="search-icon" />
                     </button>
                     <div class="header-mic">
                          <BsFillMicFill class="search-icon" />
                     </div>
                 </div>
                 <div class="header-items header-profile">
-                    <div class="header-tools"></div>
+                    {/* <div class="header-tools"></div> */}
+                    <button class="header-tools">Sign In</button>
                 </div>
             </div>
     </div>
--- frontend/src/Video.js
+++ frontend/src/Video.js
@@ -3,6 +3,50 @@
 import React, { useEffect, useState } from 'react'
 import Header from './Header'
 import tmkoc from './images/tmkoc.jpg'
+import profilePicture from './images/avtar.png'
+import {LiaThumbsDown, LiaThumbsUp, LiaDownloadSolid} from 'react-icons/lia'
+import {PiShareFatLight} from 'react-icons/pi'
+
+
+const VideoInfo = () => {
+  return (
+      <>
+          <h1 id='video-title' className='fs-5 fw-bold my-3'> Abdul Hannan & Rovalio - Iraaday (Official Music Video) </h1> 
+          <div id='top-row' className='row'>
+              <div id='owner' className='d-flex col-lg-5'>
+                  <img src={profilePicture} className='p-1' width={55} height={55}></img>
+                  <div>
+                      <h2 className='fs-5 px-1 py-1 m-0 artist-name fontsize-smaller'> Abdul Hannan </h2>
+                      <p className='px-1 m-0 text-muted artist-subscribers'>273K subscribers</p>
+                  </div>
+                  <button className='bg-black text-white fw-bold rounded-5 m-2 mx-3 p-1 px-3 subscribe-button'>Subscribe</button>
+              </div>
+              <div id='actions' className='col-lg my-2 justify-space-around'>
+                  <button className='action-button rounded-5 m-1 p-2 px-4 border-0'>
+                      <LiaThumbsUp size={'1.4rem'} /><h6 className='fontsize-small  p-1 px-2 d-inline d-lg-none d-xl-inline '>597K</h6> | <LiaThumbsDown size={'1.4rem'} />
+                  </button>
+                  <button className='action-button rounded-5 p-2 px-3 border-0'>
+                      <PiShareFatLight size={'1.4rem'}/><h6 className='fontsize-small d-inline p-1'>Share</h6>
+                  </button>
+                  <button className='action-button rounded-5 p-2 px-3 border-0 d-inline d-lg-none d-xl-inline d-xs-none d-md-inline'>
+                      <LiaDownloadSolid size={'1.4rem'}/><h6 className='fontsize-small d-inline p-1'>Download</h6>
+                  </button>
+                  <button className='action-button rounded-5 p-2 px-3 border-0 float-lg-end'>
+                      <h6>...</h6>
+                  </button>
+              </div>
+              <div className='col-sm-12 video-description rounded-4 p-2'>
+                  <h6 className='fontsize-small p-1'>434K views &nbsp; 1 year ago</h6>
+                  <p className='fontsize-smaller'>
+                      Here is the compilation video for all the random suggestions Shukla Ji gave from 
+                      the film Chhalaang. Watch this video for non stop laughter and let us know in 
+                      the comments which suggestion did you relate with the most.
+                  </p>
+              </div>
+          </div>
+      </>
+  );
+}
 
 function SuggestedVideo(){
   return (<>
@@ -48,6 +92,7 @@ function Video() {
       <div className='video-main-page'>
         <div className='video-frame'>
           <iframe className='iframe-video' src="https://www.youtube.com/embed/Grr0FlC8SQA?si=8TyAKqTfvtCYcIdO?rel=0&mute=1&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <VideoInfo />
         </div>
         <div className='suggestions'>
           <p>Suggested Videos:</p>
@@ -56,6 +101,7 @@ function Video() {
           })}
         </div>
       </div>
+
     </div>
   )
 }
